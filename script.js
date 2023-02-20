4f6f847e3eb70fa250c02b9f2d82ebe15ba03e06
Lily Silva <38285251+lily-silva@users.noreply.github.com>
2023-02-20 23:03:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,35 @@
-// open and close add new book form modal
+"use strict";
 
-const modal = document.getElementById("modal");
-const closeBtn = document.getElementById("close-btn");
-const addBook = document.getElementById("add-book-btn");
-const subBook = document.getElementById("sub-btn");
-
-const closeModal = () => {
-	modal.style.display = "none";
-};
-
-const openModal = () => {
-	modal.style.display = "block";
-};
-
-addBook.onclick = openModal;
-closeBtn.onclick = closeModal;
-
-// Toggle read/not read button
-
-const readBtn = document.getElementById("read-btn");
-const toggleRead = () => {
-	const btnLabel = readBtn.textContent;
-	if (btnLabel === "Read") {
-		readBtn.style["background-color"] = "red";
-		readBtn.textContent = "Not read";
-	} else {
-		readBtn.style["background-color"] = "#17c3b2";
-		readBtn.textContent = "Read";
-	}
-};
-
-readBtn.onclick = toggleRead;
-
-// Form validation
+// variables
 const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const btn = document.getElementById("submit");
 
-const subBtn = document.getElementById("sub-btn");
+const bookdiv = document.getElementById("book");
+const bookTitle = document.createElement("p");
+const bookAuthor = document.createElement("p");
+const bookPages = document.createElement("p");
 
-const check = () => {
-	if (title.value.length < 1) {
-		return "error error error";
-	} else {
-		return title.value;
-	}
-};
+let myLibrary = [];
 
-subBtn.addEventListener("click", function (e) {
-	console.log(check());
-});
+// methods
+
+function Book(title, author, pages) {
+	(this.title = title), (this.author = author), (this.pages = pages);
+}
+
+function addBookToLibrary(event) {
+	const book = new Book(title.value, author.value, pages.value);
+	myLibrary.push(book);
+	myLibrary.forEach((el) => {
+		bookTitle.textContent = el.title;
+		bookAuthor.textContent = el.author;
+		bookPages.textContent = el.pages;
+	});
+	bookdiv.appendChild(bookTitle);
+}
+
+// inits & event listeners
+
+btn.addEventListener("click", addBookToLibrary);
