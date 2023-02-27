78563ecd64f286a534ef6cb812640349857c3399
Lily Silva <38285251+lily-silva@users.noreply.github.com>
2023-02-27 16:34:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
 "use strict";
 
 // variables
+const form = document.getElementById("add-book")
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const btn = document.getElementById("submit");
+const read = document.getElementById("read");
+const tbr = document.getElementById("tbr");
 
-const bookdiv = document.getElementById("book");
-const bookTitle = document.createElement("p");
-const bookAuthor = document.createElement("p");
-const bookPages = document.createElement("p");
+const bookCont = document.getElementById("book-container")
+
 
 let myLibrary = [];
 
 // methods
 
-function Book(title, author, pages) {
-	(this.title = title), (this.author = author), (this.pages = pages);
+function Book(title, author, pages, read, tbr) {
+	(this.title = title), (this.author = author), (this.pages = pages), (this.read = read), (this.tbr = tbr);
 }
 
 function addBookToLibrary(event) {
-	const book = new Book(title.value, author.value, pages.value);
+
+	event.preventDefault();
+
+	let bookTitle = "";
+	let bookAuthor = "";
+	let bookPages = "";
+	let bookRead = "";
+	let bookTBR = "";
+
+	const book = new Book(title.value, author.value, pages.value, read.checked, tbr.checked);
+
 	myLibrary.push(book);
+
 	myLibrary.forEach((el) => {
-		bookTitle.textContent = el.title;
-		bookAuthor.textContent = el.author;
-		bookPages.textContent = el.pages;
+		bookTitle = el.title;
+		bookAuthor = el.author;
+		bookPages = el.pages;
+		bookRead = el.read;
+		bookTBR = el.tbr;
 	});
-	bookdiv.appendChild(bookTitle);
+
+	const newBookCard = `
+	<div class="card">
+		<div class="card-body">
+			<p class="mb-0 text-secondary fst-italic fw-light book-md">title</p>
+			<p>${bookTitle}</p>
+			<p class="mb-0 text-secondary fst-italic fw-light book-md">author</p>
+			<p>${bookAuthor}</p>
+			<p>${bookPages} <span class="text-secondary fst-italic fw-light book-md">pages</span></p>
+			<div class="d-flex col-12 gap-3">
+				<div class="form-check">
+					<input class="form-check-input" type="checkbox" name="read" id="read" ${bookRead ? "checked" : ""} />
+					<label class="form-check-label" for="read">read</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="checkbox" name="read" id="tbr" ${bookTBR ? "checked" : ""} /> <label class="form-check-label" for="tbr">to read</label>
+				</div>
+			</div>
+		</div>
+	</div>	
+	`
+	bookCont.insertAdjacentHTML( 'beforeend', newBookCard);
+
+	form.reset()
 }
 
 // inits & event listeners
 
-btn.addEventListener("click", addBookToLibrary);
+form.addEventListener("submit", addBookToLibrary)
